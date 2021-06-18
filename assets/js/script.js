498aa26ae96a4032c05b999533e2f239060c9bee
Daniel Carazo <danielcarazo20@gmail.com>
2021-06-17 20:23:11 -0400

    fixed questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,50 +4,50 @@ var quizQuestions = [
     question: "What is an array?",
     answers: [
        "A single variable that stores multiple events",
-         "s",
-       "q",
+         "It is a feature",
+       "A button",
         "IDK",
     ],
     correctAnswer: "A single variable that stores multiple events",
 },
 {
   question: "What are the two values of a Boolean?",
-  answers: {
-      a: "Yes and No",
-      b: "Now and later",
-      c: "True and False",
-      d: "IDK",
-  },
+  answers: [
+       "Yes and No",
+       "Now and later",
+       "True and False",
+       "IDK",
+  ],
   correctAnswer: "True and False",
 },
 {
   question: "What is a string?",
-  answers: {
-      a: "It is a shoelace ",
-      b: "Stores and manipulates text",
-      c: "A Guitar note",
-      d: "IDK",
-  },
+  answers: [
+       "It is a shoelace ",
+       "Stores and manipulates text",
+       "A Guitar note",
+       "IDK",
+  ],
   correctAnswer: "Stores and manipulates text",
 },
 {
   questions: "What does console.log do",
-  answers: {
-      a: "Creates an arguement",
-      b: "Creates an pnject",
-      c: "Writes a message to the console",
-      d: "IDK",
-  },
+  answers: [
+       "Creates an arguement",
+       "Creates an pnject",
+       "Writes a message to the console",
+       "IDK",
+  ],
   correctAnswer: "Writes a message to the console",
 },
 {
   questions: "What is a function?",
-  answers: {
-      a: "A building block in JS",
-      b: "An expression",
-      c: "A debugger",
-      d: "IDK",
-  },
+  answers: [
+       "A building block in JS",
+       "An expression",
+       "A debugger",
+       "IDK",
+  ],
   correctAnswer: "A building block in JS",
 }
 ];
@@ -67,12 +67,13 @@ var score = 0;
 var currentQuestion = 0
 
 // timer ***change back to 15sec***
-var timeLeft = 10;
+var timeLeft = 15;
 var holdTime = 0;
 
+var btn;
 
 function checkAnswer(){
-  console.log('test', this.value);
+  // console.log('test', this.value);
   var choice = this.value;
   var correctAn = quizQuestions[currentQuestion].correctAnswer;
   if(choice === correctAn ){
@@ -83,8 +84,8 @@ function checkAnswer(){
   }
 
   currentQuestion++;
+  
   startQuiz();
-
 }
 
 // start quiz function 
@@ -93,30 +94,29 @@ function startQuiz () {
  //render question
   var quest = quizQuestions[currentQuestion].question;
   questArea.textContent = quest;
-
+  // console.log(quest);
 
  //render choices as buttons
  var options = quizQuestions[currentQuestion].answers;
- console.log(options)
+//  console.log('currentQuestion = ', currentQuestion);
+//  console.log('options = ', options);
+
+// console.log(options.length);
 
  for (let i = 0; i < options.length; i++) {
-   var btn = document.createElement('button');
+   btn = document.createElement('button');
    btn.value = options[i];
    btn.textContent = options[i];
+  //  console.error(options[i]);
    btn.onclick = checkAnswer;
    choices.appendChild(btn);
-   
- }
+  }
  //     -- create a button
  //     --assign it value
  //     --assign it context
  //     --assign it an onclick function
  //         ..check if user answered correctly or not
   
-};
-
-// question prompt function
-function testQuestions () {
 };
 
 // show quiz result function 
@@ -132,7 +132,6 @@ startScreen.className = 'hide';
 //      --show quizArea
 quizArea.className = 'show';
 
-
   startQuiz();
 
 
@@ -140,7 +139,7 @@ quizArea.className = 'show';
     holdTime = setInterval(function() {
       timeLeft--;
       holdTime.textContent = "You have " + timeLeft;
-      timerEl.textContent =timeLeft
+      timerEl.textContent = timeLeft
 
       if (timeLeft < 0) {
         clearInterval(holdTime);
@@ -151,13 +150,3 @@ quizArea.className = 'show';
     
   }
 })
-
-
-// // end message after clicking start
-// var message = "Let's see how you did!";
-// var words = message.split (' ');
-
-// start quiz function
-//startQuiz();
-
-// startBtnEl.addEventListener("click", submit);
