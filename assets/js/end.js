e1e8fd9a4db72d2921508831e3534822b551d170
Daniel Carazo <danielcarazo20@gmail.com>
2021-06-25 15:59:29 -0400

    edited end game html and js

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,31 +1,22 @@
-var emailInput = document.querySelector('#email');
-var passwordInput = document.querySelector('#password');
-var signUpButton = document.querySelector('#sign-up');
+var saveButton = document.querySelector('#save');
 var msgDiv = document.querySelector('#msg');
-var userEmail = document.querySelector('#user-email');
-var userPassword = document.querySelector('#user-password');
-
 
 function displayMessage(type, message) {
   msgDiv.textContent = message;
   msgDiv.setAttribute('class', type);
 }
 
-signUpButton.addEventListener('click', function(event) {
+saveButton.addEventListener('click', function(event) {
   event.preventDefault();
 
   var email = document.querySelector('#email').value;
-  var password = document.querySelector('#password').value;
 
   if (email === '') {
-    displayMessage('error', 'Email cannot be blank');
-  } else if (password === '') {
-    displayMessage('error', 'Password cannot be blank');
+    displayMessage('error', 'Cannot be blank');
   } else {
-    displayMessage('success', 'Registered successfully');
+    displayMessage('success', 'Stats saved');
 
     // Save email and password to localStorage using `setItem()`
-    localStorage.setItem('email', email);
-    localStorage.setItem('password', password);
+    localStorage.setItem('stats', email);
   }
 });
